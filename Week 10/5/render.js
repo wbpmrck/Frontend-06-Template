60e910ff836c97526eb032d81013d5214ab5a667
kaicui <wbpmrck2003@gmail.com>
2021-01-01 18:04:55 +0800

    finish render.all work 10 finished

---
+++ Week 10/5/render.js
@@ -0,0 +1,18 @@
+const images = require('images');
+
+
+//目前render方法仅仅处理背景色
+function render(viewport,element){
+  if(element.style){
+    let img = images(element.style.width,element.style.height);
+
+    if(element.style["background-color"]){
+      let color = element.style["background-color"] || "rgb(0,0,0)";
+      color.match(/rgb\((\d+),(\d+),(\d+)\)/);
+      img.fill(Number(RegExp.$1),Number(RegExp.$2),Number(RegExp.$3),1);
+      viewport.draw(img,element.style.left||0,element.style.top||0);
+    }
+  }
+}
+
+module.exports = render;
--- Week 10/server.js
+++ Week 10/server.js
@@ -20,12 +20,16 @@ http.createServer( (req,resp)=>{
       width:500px;
       height:300px;
       display:flex;
+      background-color: rgb(255,255,255);
     }
     #container #myid{
       width:200px;
+      height:100px;
+      background-color: rgb(255,0,0);
     }
     #container .c1 {
       flex:1;
+      background-color: rgb(0,255,0);
     }
   </style>
 </head>
